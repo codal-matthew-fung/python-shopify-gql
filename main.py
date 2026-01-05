859a5a548a19a5b05a78630c41428efe9aa360c4
codal-matthew-fung <mfung@codal.com>
2026-01-05 11:57:58 +0000

    CIT-1015 Implement loop to get all products

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
+def extract_all_products(data):
+    all_products = []
+    hasNextPage = True
+    cursor = None
+
+    print("Extracting products...")
+
+    while hasNextPage:
+        variables = {"cursor": cursor}
+        response = client.execute_query(products_query, variables)
+
+        if not response:
+            break
+        
+        data = response['data']['products']
+        nodes = [edge['node'] for edge in data['edges']]
+
+        all_products.extend(nodes)
+
+        pageInfo = data['pageInfo']
+        hasNextPage = pageInfo['hasNextPage']
+        cursor = pageInfo['endCursor']
+
+        print(f"Fetched {len(all_products)} products so far...")
+
+    return all_products
+
 from shopify_client import ShopifyClient
 
 client = ShopifyClient()
 
-test_query = """
-{
-  shop {
-    name
-    primaryDomain {
-      url
+products_query = """
+query GetProducts($cursor: String) {
+  products(first: 50, after: $cursor) {
+    pageInfo {
+      hasNextPage
+      endCursor
     }
-  }
-  products(first: 3) {
-    nodes {
-      id
-      title
+    edges {
+      node {
+        id
+        title
+        handle
+        vendor
+      }
     }
   }
 }
 """
 
-data = client.execute_query(test_query)
-print(f"Connected to: {data['data']['shop']['name']}")
+product_list = extract_all_products(client)
 
-products = data['data']['products']['nodes']
-print("First 3 Products:")
-for product in products:
-    print(f"- {product['title']} (ID: {product['id']})")
+print(f"Total products extracted: {len(product_list)}")
